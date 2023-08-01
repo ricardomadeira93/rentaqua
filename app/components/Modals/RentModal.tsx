2d5cf3798e412e526793f074d841826f6471ba73
ricardomadeira93 <ricardomadeira1993@gmail.com>
2023-08-01 14:11:52 +0100

    Rent Modal

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -1,11 +1,36 @@
-"use client"
+'use client';
 
 import useRentModal from '@/app/hooks/useRentModal';
 
 import Modal from './Modal';
+import { useState } from 'react';
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGE$ = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
   const rentModal = useRentModal();
+
+  const [step, setStep] = useState(
+    STEPS.CATEGORY
+  );
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+
+// Continuar aqui 3:22
+
   return (
     <div>
       <Modal
